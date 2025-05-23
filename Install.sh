3aa23ae624cb87db6119be71fc0c522bd6d4e231
magomarto <lucashravazi55@gmail.com>
2025-05-22 23:50:29 -0300

    script

--- Install.sh
+++ Install.sh
@@ -1,12 +1,25 @@
-#!/bin/bash
+#!/bin/bash -S bash -e
 
 #USERNAME=$(whoami)
+
+clear
 
 # DOTS
 HYPRLAND_CONFIG_DIR="$HOME/.config/hypr"
 WAYBAR_CONFIG_DIR="$HOME/.config/waybar"
 WOFI_CONFIG_DIR="$HOME/.config/wofi"
 KITTY_CONFIG_DIR="$HOME/.config/kitty"
+
+# colours for text
+
+BOLD='\e[1m'
+BRED='\e[91m'
+BBLUE='\e[34m'
+BGREEN='\e[92m'
+BYELLOW='\e[93m'
+RESET='\e[0m'
+
+REPO_DIR=$(dirname "$0")
 
 # MY DOCUMENTS
 
@@ -31,12 +44,15 @@ FONTPACKAGES=(
 )
 
 USERPACKAGES=(
+	bluez
+	bluez-utils
 	gvfs 
 	thunar 
 	thunar-volman 
 	libreoffice 
 	firefox 
-	keepassxc 
+	keepassxc
+	btop 
 	fastfetch 
 	unzip 
 	spotify-launcher
@@ -53,6 +69,12 @@ NETWORKPACKAGES=(
 	ufw
 )
 
+AURPACKAGES=(
+
+	visual-studio-code-bin
+	brave-bin
+)
+
 
 checkError(){ #FUNÇÃO QUE VERIFICA ERROS
 	if [ $? -ne 0 ]; then
@@ -64,101 +86,83 @@ checkError(){ #FUNÇÃO QUE VERIFICA ERROS
 
 echo "=== Creating Workspace Directorys ===" #falta
 
+mdkir Images
+mkdir Documents
+cd Documents
+mkdir Projects Poison 
+cd ..
+
 
 
 echo "=== Updating the system  ==="
 sudo pacman -Syu --noconfirm
 checkError "!!! System Update Failed !!!"
 
-echo "=== Packages Installation  ==="
+echo "=== Pacman Packages Installation  ==="
 sudo pacman -S --noconfirm \
 	$WORKSPACEPACKAGES \
 	$FONTPACKAGES \
 	$USERPACKAGES \
 	$DEVPACKAGES\
 	$NETWORKPACKAGES
-checkError "!!! Package installation failed !!!"
-	
+checkError "!!! Pacman Package installation failed !!!"
+
+
+echo "=== Yay Setup ==="
+git clone https://aur.archlinux.org/yay.git
+cd yay 
+makepkg -si
+cd .. 
+rm -r yay
+
+echo "=== AUR Package Installation with yay ==="
+checkError "!!! AUR Package Setup Failled !!!"
+yay -S --noconfirm/
+	$AURPACKAGES
+
+checkError "=== yay package installation failed ==="
+
 
 echo "=== Removing Useless Packages ===" #falta
 	
 sudo pacman -Rns dolphin
 
-echo "=== Hyprland Setup ===" #falta
 
-echo "=== Hyprpaper Setup ===" #falta
-cat > $HYPRLAND_CONFIG_DIR/hyprpaper.conf << EOF
+echo "=== Copiando/Atualizando dotfiles ==="
 
-EOF
-checkError "!!! Hyprpaper Setup Failed !!!"
+# Hyprland (hyprland.conf + hyprpaper.conf)
+rsync -av "$REPO_DIR/hypr/" "$HYPRLAND_CONFIG_DIR/"
+checkError "!!! Falha ao atualizar Hyprland !!!"
+
+# Waybar (config + style.css)
+rsync -av "$REPO_DIR/waybar/" "$WAYBAR_CONFIG_DIR/"
+checkError "!!! Falha ao atualizar Waybar !!!"
+
+# Wofi (config + style.css)
+rsync -av "$REPO_DIR/wofi/" "$WOFI_CONFIG_DIR/"
+checkError "!!! Falha ao atualizar Wofi !!!"
+
+# Kitty (kitty.conf)
+rsync -av "$REPO_DIR/kitty/" "$KITTY_CONFIG_DIR/"
+checkError "!!! Falha ao atualizar Kitty !!!"
+
+# Wallpapers (diretório separado)
+rsync -av "$REPO_DIR/wallpapers/" "$HYPRLAND_CONFIG_DIR/wallpapers/"
+checkError "!!! Falha ao copiar wallpapers !!!"
 
 
-echo "=== Wofi Setup ===" #falta
-mkdir -p $WOFI_CONFIG_DIR 
-cat > $WOFI_CONFIG_DIR/config << EOF
-# CONFIG do wofi aqui
-EOF
-cat > $WOFI_CONFIG_DIR/style.css << EOF
-# ESTILO DO WOFI AQUI
-EOF
-checkError "!!! Wofi Setup Failed !!!"
 
-echo "=== Waybar Setup ===" #falta
-mkdir -p $WAYBAR_CONFIG_DIR
-cat > $WAYBAR_CONFIG_DIR/config << EOF
-#CONFIG DO WAYBAR AQUI
-EOF
-cat > $WAYBAR_CONFIG_DIR/style.css << EOF
-# ESTILO DA WAYBAR AQUI
-EOF
-checkError "!!! Waybar Setup Failed !!!"
 
-echo "=== Kitty Setup ===" # FEITO
-mkdir -p $KITTY_CONFIG_DIR
-cat > $KITTY_CONFIG_DIR/kitty.conf << EOF
-background #0a0a0a
-foreground #ffeedd
-selection_background #333333
-selection_foreground #ebdbb2
 
-color0 #1a1a1a
-color8 #333333
-color1 #689d6a
-color9 #8ec07c
-color2 #98971a
-color10 #b8bb26
-color3 #d79921
-color11 #fabd2f
-color4 #458588
-color12 #83a598
-color5 #b16286
-color13 #d3869b
-color6 #1793D1
-color14 #5fd1fa
-color7 #ebdbb2
-color15 #ffeedd
 
-cursor #fba02f
-cursor_text_color #ffffff
-url_color #458588
 
-window_padding_width 8
-active_border_color #cc241d
-inactive_border_color #333333
-bell_border_color #cc241d
 
-background_opacity 0.85
-dynamic_background_opacity yes
-hide_window_decorations titlebar-only
 
-font_family Hack
-bold_font Hack Bold
-italic_font Hack Italic
-bold_italic_font Hack Bold Italic
 
-font_size 9
-EOF
-checkError "!!! Kitty setup failed !!!"
+
+
+
+
 
 echo"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣠⣤⣤⣤⣤⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
 echo"⠀⠀⠀⠀⠀⠀⠀⣠⡴⠞⠛⠉⠉⠉⠀⠈⠉⠉⠙⠛⠷⢦⣀⠀⠀⠀⠀⠀⠀⠀"
